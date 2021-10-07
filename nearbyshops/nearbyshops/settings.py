aa079782d8ea80a2519892fd27fe64a128988806
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-10-07 21:12:14 +0200

    updated

--- nearbyshops/nearbyshops/settings.py
+++ nearbyshops/nearbyshops/settings.py
@@ -76,9 +76,7 @@ WSGI_APPLICATION = 'nearbyshops.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES={
-   'default':{
-      '*********'
-   }
+   '****'
 }
 
 
